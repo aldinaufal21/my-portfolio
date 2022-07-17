523637d90043696c4d30170a236997fc884fa587
aldinaufal21 <ry.prg75@gmail.com>
2022-07-17 11:03:18 +0700

    version 1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#06b6d4',
+        dark: '#0f172a',
+        secondary: '#64748b',
+      }
+    },
+  },
+  plugins: [],
+}
