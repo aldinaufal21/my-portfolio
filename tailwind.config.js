c316842b0c48232434a07f0b254f3f131f901908
Aldy Naufal <aldy@homeez.com>
2026-09-08 12:39:24 +0700

    feat: generate Tailwind CSS output file and configure project settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./*.html", "./public/**/*.js", "./src/**/*.js"],
   theme: {
     container: {
       center: true,
